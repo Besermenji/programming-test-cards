13e6a805361321897dc6689e4401705edc9d65f7
Vladimir Besermenji <besermenji.vladimir@gmail.com>
2020-08-24 15:44:22 +0200

    Updates

---
+++ precache-manifest.a7b93f1874e40783c446572ff2ded331.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d0850c512cdf7fdf2483739637015e",
+    "url": "/programming-test-cards/index.html"
+  },
+  {
+    "revision": "ecaa39752bbfb47decff",
+    "url": "/programming-test-cards/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6661752808d47f693ddc",
+    "url": "/programming-test-cards/static/js/2.d11fba5f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/programming-test-cards/static/js/2.d11fba5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecaa39752bbfb47decff",
+    "url": "/programming-test-cards/static/js/main.2e292ce3.chunk.js"
+  },
+  {
+    "revision": "ca90f7c04e76f120f44e",
+    "url": "/programming-test-cards/static/js/runtime-main.c130444b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/programming-test-cards/static/media/logo.5d5d9eef.svg"
+  }
+]);
